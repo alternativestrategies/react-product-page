d2b0df5ad693ffb1f12d7ec9650607852d689ede
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-08-16 13:26:16 -0400

    fix routes (hopefully)

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,13 +12,13 @@ const Navigation = () => {
             <Navbar.Collapse>
                 <Nav className="ml-auto" >
                     <Nav.Item >
-                    <A href="/" className={path === "/" ? 'active' : 'normal'} >Home</A>
+                    <A href="/react-product-page/" className={path === "/" ? 'active' : 'normal'} >Home</A>
                     </Nav.Item>
                     <Nav.Item >
-                    <A href="/products" className={path === "/products" ? 'active' : 'normal'}>Products</A>
+                    <A href="/react-product-page/products" className={path === "/products" ? 'active' : 'normal'}>Products</A>
                     </Nav.Item>
                     <Nav.Item >
-                    <A href="/contact" className={path === "/contact" ? 'active' : 'normal'}>Contact</A>
+                    <A href="/react-product-page/contact" className={path === "/contact" ? 'active' : 'normal'}>Contact</A>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
